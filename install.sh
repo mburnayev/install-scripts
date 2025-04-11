4ef5ddaca6d62cdd404e12ae13d19c215584c096
Misha <82725378+mburnayev@users.noreply.github.com>
2025-04-11 12:53:13 -0400

    minimal install.sh update

--- install.sh
+++ install.sh
@@ -9,25 +9,16 @@ brew install gcc
 
 # --- Python ---
 brew install python3
-# pip should come w/ Python versions >= 3.4 
 
 # --- Java ---
 brew install java
 
 # --- Dart & Flutter --- 
-brew tap dart-land/dart
-brew install dart
-sudo softwareupdate --install-rosetta --agree-to-license
+# brew tap dart-land/dart
+# brew install dart
+# sudo softwareupdate --install-rosetta --agree-to-license
 # Flutter bundle needs to be downloaded and extracted into project location
 # unzip ~/Downloads/flutter_macos_arm64_3.7.11-stable.zip
 # mv flutter ../Documents
 # rm flutter_macos_arm64_3.7.11-stable.zip
 # export PATH="$PATH:`pwd`/flutter/bin"
-
-# --- Lua ---
-
-# --- Rust ---
-
-# --- Miscellaneous ---
-#drawio
-
